bc294ec65c69ff63ec88a0a46913d0eb66fc6de0
JD Tadlock <jt7903@gmail.com>
2023-11-09 14:41:12 -0500

    auth complete

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,10 +1,20 @@
 const User = require('../models/User');
 
+const { createToken } = require('./helpers');
+
 const user_controller = {
   // Register a user
   async register(req, res) {
     try {
       const user = await User.create(req.body);
+
+      const token = await createToken(user._id);
+
+      // Authenticate/Log In User
+      res.cookie('token', token, {
+        maxAge: 60 * 60 * 1000, // 1 hour
+        httpOnly: true
+      });
 
       res.json(user);
     } catch (err) {
@@ -13,6 +23,40 @@ const user_controller = {
         message: err.message
       })
     }
+  },
+
+  // Log user in
+  async login(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(403).send({
+      message: 'User with that email address not found.'
+    });
+
+    const pass_id_valid = await user.validatePass(password);
+
+    if (!pass_id_valid) return res.status(403).send({
+      message: 'Password is invalid.'
+    });
+
+    const token = await createToken(user._id);
+
+    res.cookie('token', token, {
+      maxAge: 60 * 60 * 1000,
+      httpOnly: true
+    });
+
+    res.json(user);
+  },
+
+  // Test protected method
+  protected(req, res) {
+    res.json({
+      user: req.user,
+      authenticated: true
+    })
   }
 };
 
--- server/routes/user_routes.js
+++ server/routes/user_routes.js
@@ -2,7 +2,15 @@ const router = require('express').Router();
 
 const user_controller = require('../controllers/user_controller');
 
+const { isAuthenticated } = require('../controllers/helpers');
+
 // Register user
 router.post('/register', user_controller.register);
+
+// Login user
+router.post('/login', user_controller.login);
+
+// Protected test route
+router.get('/protected', isAuthenticated, user_controller.protected);
 
 module.exports = router;
